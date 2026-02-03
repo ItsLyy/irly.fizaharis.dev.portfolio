ed587816205bd5cd3dba5af243ffdf3b03d2d793
irly.dev <irly.fizaharis.dev@gmail.com>
2026-02-03 21:20:09 +0700

    Fix bug

--- app/_db/index.ts
+++ app/_db/index.ts
@@ -1,8 +1,11 @@
 /**
  * Node Modules
  */
+import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
+
+config({ path: ".env" });
 
 const client = postgres(process.env.DATABASE_URL!);
 export const db = drizzle({ client, casing: "snake_case" });
